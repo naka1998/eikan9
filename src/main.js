6e8c3f761250d06c90001f926dc73667f4de10af
naka1998 <bjtn1290411@gn.iwasaki.ac.jp>
2019-07-19 14:08:42 +0900

    jsonに応じて描画

--- src/main.js
+++ src/main.js
@@ -7,6 +7,96 @@ import PlayerCard from "./mainpage/player";
 class Main extends React.Component {
   constructor() {
     super();
+    this.posname = ["なし", "投手", "捕手", "一塁手", "二塁手", "三塁手", "遊撃手", "外野手"];
+    this.persname = ["天才肌", "ごくふつう", "お調子者", "やんちゃ", "クール", "したたか", "内気", "熱血漢"];
+    this.persPitcher =
+      ["全体的(めっちゃ伸びる)", "全体的(平均的)", "全体的(平均的)", "球速", "コントロール, 投球フォーム", "変化球覚える, 変化球みがく", "コントロール, 変化球覚える, 変化球みがく", "球速, スタミナ"];
+    this.persFielder =
+      ["全体的(めっちゃ伸びる)", "全体的(平均的)", "走力, 肩力", "弾道, パワー", "ミート, 守備位置変更, 打席変更, 打撃フォーム", "守備力, エラー回避", "エラー回避", "パワー"];
+    this.fielderTokunou = [
+      "チャンス",
+      "対左投手",
+      "キャッチャー",
+      "盗塁",
+      "走塁",
+      "送球",
+      "秋男",
+      "夏男",
+      "春男",
+      "ケガしにくさ",
+      "アベレージヒッター",
+      "パワーヒッター",
+      "プルヒッター",
+      "広角打法",
+      "意外性",
+      "いぶし銀",
+      "お祭り男",
+      "固め打ち",
+      "逆境◯",
+      "プレッシャーラン",
+      "高速チャージ",
+      "サヨナラ男",
+      "守備職人",
+      "初球◯",
+      "対エース◯",
+      "代打◯",
+      "チャンスメーカー",
+      "内野安打◯",
+      "流し打ち",
+      "粘り打ち",
+      "ハイボールヒッター",
+      "ローボールヒッター",
+      "バント◯",
+      "バント職人",
+      "ホーム死守",
+      "ヘッドスライディング",
+      "満塁男",
+      "ムード◯",
+      "レーザービーム",
+      "△△キラー",
+      "ダメ押し",
+      "インコースヒッター",
+      "アウトコースヒッター",
+      "かく乱",
+      "悪球打ち",
+      "国際大会◯",
+      "対変化球◯",
+      "ホーム突入",
+    ];
+    this.pitcherTokunou = [
+      "対ピンチ",
+      "対左打者",
+      "打たれ強さ",
+      "ノビ",
+      "クイック",
+      "回復",
+      "威圧感",
+      "重い球",
+      "勝ち運",
+      "緩急◯",
+      "キレ◯",
+      "緊急登板◯",
+      "クロスファイヤー",
+      "牽制◯",
+      "根性◯",
+      "ジャイロボール",
+      "尻上がり",
+      "対強打者◯",
+      "打球反応◯",
+      "奪三振",
+      "球持ち◯",
+      "闘志",
+      "内角攻め",
+      "逃げ球",
+      "低め◯",
+      "リリース◯",
+      "△△キラー",
+      "球速安定",
+      "ポーカーフェイス",
+      "力配分",
+      "国際大会◯",
+      "回またぎ◯",
+    ];
     this.state = {
       sort: 0,
       filter: {
@@ -22,8 +112,8 @@ class Main extends React.Component {
   }
   readJson() {
     //    data = JSON.parse(data);
-    console.log(data);
-    this.setState = { player: data, };
+    console.log(data["player"][0]);
+
   }
   writeJson() {
 
@@ -51,12 +141,32 @@ class Main extends React.Component {
   changeSortState(e) {
     this.setState({ sort: e.target.value });
   }
-  renderPlayercard() {
+  renderPlayercard(i) {
+    console.log(data["player"][i]["year"]);
+    const pos1 = this.posname[data["player"][i]["pos1"]];
+    const pos2 = this.posname[data["player"][i]["pos2"]];
+    const pos3 = this.posname[data["player"][i]["pos3"]];
+    const pers = this.persname[data["player"][i]["pers"]];
+    const persAbility = data["player"][i]["isPitcher"] ? this.persPitcher[data["player"][i]["pers"]] : this.persFielder[data["player"][i]["pers"]];
+    const tokunou = data["player"][i]["isPitcher"] ?
+      data["player"][i]["tokunou"].map((i) => this.pitcherTokunou[i]) + "" :
+      data["player"][i]["tokunou"].map((i) => this.fielderTokunou[i]) + "";
     return (
-      <PlayerCard />
+      <PlayerCard
+        year={data["player"][i]["year"]}
+        name={data["player"][i]["name"]}
+        pos1={pos1}
+        pos2={pos2}
+        pos3={pos3}
+        pers={pers}
+        persAbility={persAbility}
+        tokunou={tokunou}
+        policy={data["player"][i]["policy"]}
+      />
     );
   }
   render() {
+    this.readJson();
     return (
       <div>
         <header>
@@ -65,8 +175,7 @@ class Main extends React.Component {
         </header>
         <div id="wrap">
           <div id="playerwrap">
-            {this.renderPlayercard
-            }
+            {data["player"].map((value, index) => this.renderPlayercard(index))}
           </div>
         </div>
       </div>
--- src/mainpage/player.js
+++ src/mainpage/player.js
@@ -5,12 +5,18 @@ class PlayerCard extends React.Component {
   render() {
     return (
       <div className="playerCard">
-        <p className="cardYear">2017年</p>
-        <p className="cardName">銀次</p>
-        <p className="cardPos">一塁手・二塁手</p>
-        <p className="cardPers">やんちゃ → 弾道・パワー </p>
-        <p className="cardTokunou">特殊能力</p>
-        <p className="cardPolicy">育成方針</p>
+        <p className="cardYear">{this.props.year + "年"}</p>
+        <p className="cardName">{this.props.name}</p>
+        <div className="cardPos">
+          <p className="cardPos1">{"メイン: " + this.props.pos1}</p>
+          <p className="cardPos2">{"サブ: " + this.props.pos2}</p>
+          <p className="cardPos3">{"サブ: " + this.props.pos3}</p>
+        </div>
+        <p className="cardPers">
+          {"性格: " + this.props.pers + "→" + this.props.persAbility}
+        </p>
+        <p className="cardTokunou">{this.props.tokunou}</p>
+        <p className="cardPolicy">{this.props.policy}</p>
       </div>
     );
   }
